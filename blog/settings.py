7ab2a9eafcf9e6c17fa18b8e631bda44ee18c01a
cadiente-jomel <cadientejomel0@gmail.com>
2021-01-10 20:19:43 +0800

    fixed typo inside settings

--- blog/settings.py
+++ blog/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['devblog023.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['devblog023.herokuapp.com', 'localhost']
 
 
 # Application definition
